55c3d07611fa895e6cf89fdc03f604dcdc796270
Navneeth <ksnavneeth614@gmail.com>
2025-10-29 15:20:42 +0530

    set up neon/postresql with drizzle

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+import { config } from "dotenv"
+export default{
+    schema : './src/models/*.js',
+    out : './drizzle',
+    dialect : 'postgresql',
+    dbCredentials : {
+        url : process.env.DATABASE_URL,
+    }
+}
